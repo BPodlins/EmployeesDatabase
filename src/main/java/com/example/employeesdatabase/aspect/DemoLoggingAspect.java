9a013bdf4ec2081b6d1cad352b281334e07d2811
XAfker <podlinskibartosz@gmail.com>
2023-10-10 23:06:31 +0200

    Finished

---
+++ src/main/java/com/example/employeesdatabase/aspect/DemoLoggingAspect.java
@@ -0,0 +1,65 @@
+package com.example.employeesdatabase.aspect;
+
+import com.mysql.cj.x.protobuf.MysqlxExpr;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class DemoLoggingAspect {
+
+    private Logger myLogger = Logger.getLogger(getClass().getName());
+
+    @Pointcut("execution(* com.example.employeesdatabase.controller.*.*(..))")
+    private void forControllerPackage() {
+
+    }
+
+    @Pointcut("execution(* com.example.employeesdatabase.service.*.*(..))")
+    private void forServicePackage() {
+
+    }
+
+    @Pointcut("execution(* com.example.employeesdatabase.dao.*.*(..))")
+    private void forDaoPackage() {
+
+    }
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+    private void forAppFlow() {
+
+    }
+
+    @Before("forAppFlow()")
+    public void before(JoinPoint theJoinPoint) {
+
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("=====> in @Before: calling method: " + theMethod);
+
+
+        Object[] args = theJoinPoint.getArgs();
+
+        for (Object tempArg : args) {
+            myLogger.info("=====>> argument: " + tempArg);
+        }
+
+    }
+
+    @AfterReturning(
+            pointcut = "forAppFlow()",
+            returning = "theResult")
+    public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+
+        String theMethod = theJoinPoint.getSignature().toShortString();
+        myLogger.info("=====> in @AfterReturning: calling method: " + theMethod);
+
+        myLogger.info("====>> result: " + theResult);
+    }
+
+}
---
+++ src/main/java/com/example/employeesdatabase/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.example.employeesdatabase.service;
+
+
+import com.example.employeesdatabase.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+	List<Employee> findAll();
+	
+	Employee findById(int theId);
+	
+	void save(Employee theEmployee);
+	
+	void deleteById(int theId);
+	
+}
